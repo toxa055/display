6257e9eaa94825e93d99eef9982fd59848f9a445
toxa055 <toxa055@gmail.com>
2021-11-12 18:06:43 +0300

    Add EventsBean class.

---
+++ src/main/java/EventsBean.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class EventsBean {
+
+    private List<Event> events;
+
+    public EventsBean() {
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+}
